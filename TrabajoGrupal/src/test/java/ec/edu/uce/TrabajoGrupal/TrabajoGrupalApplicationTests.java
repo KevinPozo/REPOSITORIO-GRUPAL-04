de17beec990002d3cdd1a3e2236afe4d98fa8df2
Kevin Pozo <kevinferpozo@gmail.com>
2024-05-28 07:40:02 -0500

    Proyecto Galaga

---
+++ TrabajoGrupal/src/test/java/ec/edu/uce/TrabajoGrupal/TrabajoGrupalApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.TrabajoGrupal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoGrupalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
