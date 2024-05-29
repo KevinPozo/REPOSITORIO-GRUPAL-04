e3429e5af228df8cb0005bdb942a3049d2393f62
Kevin Pozo <kevinferpozo@gmail.com>
2024-05-29 07:16:47 -0500

    Project Galaga Game

---
+++ GalagaProject/src/main/java/ec/edu/uce/GalagaProject/GalagaProjectApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.GalagaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalagaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalagaProjectApplication.class, args);
+	}
+
+}
---
+++ GalagaProject/src/test/java/ec/edu/uce/GalagaProject/GalagaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.GalagaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalagaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
